b468371df27bb84fffedca69db7eb6317aafad66
Ekaanth <akanth1994@gmail.com>
2024-06-15 09:59:32 +0530

    add auth guard for wallet controller

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -3,12 +3,29 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
 import { Wallet, WalletSchema } from './wallet.schema';
+import { Waitlist, WaitlistSchema } from 'src/waitlist/waitlist.schema';
+import { User, UserSchema } from 'src/users/user.schema';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { WaitlistService } from 'src/waitlist/waitlist.service';
+import { AuthService } from 'src/auth/auth.service';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Wallet.name, schema: WalletSchema }]),
+    MongooseModule.forFeature([
+      { name: Wallet.name, schema: WalletSchema },
+      { name: Waitlist.name, schema: WaitlistSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+    JwtModule.register({}),
   ],
-  providers: [WalletService],
+  providers: [
+    WalletService,
+    WaitlistService,
+    JwtService,
+    AuthService,
+    UsersService,
+  ],
   controllers: [WalletController],
   exports: [WalletService],
 })
